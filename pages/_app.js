aff28baf1548e7817eef1091a393ae3f3dc052b4
FrancoPlomer <franko.plomer@gmail.com>
2024-01-29 20:10:20 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+//Incorpora una cabecera para nuestra app
+
+import Head from 'next/head'
+
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0,width=device-width"/>
+      <title>Next-tailwind-vercel</title>
+      <meta key="description" name="description" content="Netx-tailwind-vercel"/>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
